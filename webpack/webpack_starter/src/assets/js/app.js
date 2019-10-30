614c50bdbd0b063485fd5a11bb7b8daad31a7857
강희연 <the.onlyeon@gmail.com>
2019-10-30 18:54:17 +0900

    Add Webpack 실습 파일들 추가

---
+++ webpack/webpack_starter/src/assets/js/app.js
@@ -0,0 +1,9 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+console.log(add(10, 5));
+console.log(subtract(10, 5));
+console.log(multiply(10, 5));
+console.log(divide(10, 5));
---
+++ webpack/webpack_starter/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/assets/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'assets/js/app.bundle.js'
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: __dirname + '/dist',
+    port: 3000
+    // __dirname: 현재 위치의 디렉토리 네임
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+      // node_modules에 추가된 스크립트는 제외시킨다
+    }]
+  }
+};
